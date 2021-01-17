6b7f407751818875ee1693c31f3cc4649953e366
akashdevgan <akashdevgan95@gmail.com>
2021-01-17 18:41:05 -0500

    fixed about section spacing

--- src/sections/about/about.js
+++ src/sections/about/about.js
@@ -11,19 +11,32 @@ const About = () => {
     return (
         <Container className='about'>
             <Row className='full-vh-height align-items-center'>
-                <Col xs={{ size: 10, offset: 1 }} className='offset-md-2 offset-lg-1'>
+                <Col xs={{ size: 10, offset: 1 }} md={{ size: 10, offset: 1 }} lg={{ size: 8, offset: 2 }}>
                     <Row>
                         <Col>
                             <SectionHeading title='About me'/>
                         </Col>
                     </Row>
-                    <Row className='mt-5'>
-                        <Col xs={{ size: 10 }} md={{ size: 6, offset: 0 }}>
+                    <Row className='mt-md-4'>
+                        <Col className='mt-4 mt-md-0' xs={{ size: 12 }} md={{ size: 7 }} lg={{ size: 7 }}>
+                            <p className='gray-color-text'>Hello! my name is Akash Devgan. I’m a web developer based in Toronto, Canada. </p>
+                            <p className='gray-color-text'>I’m super passiante about build things for the web. My goal is always to build applications that not only load fast but also deliver great user experience.</p>
+                            <p className='gray-color-text'>Some of my favorite techologies to work with are :</p>
+                            <div className='technologies'>
+                                <span className='gray-color-text'>Nodejs</span>
+                                <span className='gray-color-text'>React</span>
+                                <span className='gray-color-text'>Express</span>
+                                <span className='gray-color-text'>Shopify</span>
+                                <span className='gray-color-text'>JavaScript</span>
+                                <span className='gray-color-text'>Sass</span>
+                            </div>
+                        </Col>
+                        <Col className='mt-5 ml-lg-4' xs={{ size: 12 }} md={{ size: 4, offset:1 }} lg={{ size: 4, offset:0 }}>
                            <div className='image-container'> 
-                           <svg className='top-border' viewBox="0 0 202 201" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <line x1="4.03119" y1="4.96856" x2="5.53421" y2="196.963" stroke="#66FCF1" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
-                                <line x1="198" y1="5.00251" x2="5.99995" y2="5.00003" stroke="#66FCF1" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
-                            </svg>
+                                <svg className='top-border' viewBox="0 0 202 201" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <line x1="4.03119" y1="4.96856" x2="5.53421" y2="196.963" stroke="#66FCF1" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
+                                    <line x1="198" y1="5.00251" x2="5.99995" y2="5.00003" stroke="#66FCF1" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
 
                                 <img className='img-fluid' src={MyImage} /> 
                                 <svg className='bottom-border'  viewBox="0 0 201 202" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -31,11 +44,6 @@ const About = () => {
                                     <line x1="196.018" y1="196" x2="196.018" y2="4" stroke="#66FCF1" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
                                 </svg>
                            </div>
-                        </Col>
-                        <Col xs={{ size: 10 }} sm={{ size: 6, offset: 0 }}>
-                            <p className='gray-color-text'>Hello! my name is Akash Devgan. I’m a web developer based in Toronto, Canada. </p>
-                            <p className='gray-color-text'>I’m super passiante about build things for the web. My goal is always to build applications that not only load fast but also deliver great user experience.</p>
-                            <p className='gray-color-text'>Some of my favourite techologies to work with are :</p>
                         </Col>
                     </Row>
                 </Col>
