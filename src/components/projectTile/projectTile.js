df160eaea0c9b300884274c8e752f808c22204b2
akashdevgan <akashdevgan95@gmail.com>
2021-01-26 21:57:57 -0500

    finished projects section

--- src/components/projectTile/projectTile.js
+++ src/components/projectTile/projectTile.js
@@ -7,14 +7,17 @@ import linkImage from '../../images/link.svg';
 const ProjectTile = ({title,description,technologies}) => {
     return (
         <div className='project-tile p-3'>
-         
-                <h4 className='gray-color-text'>{title}</h4>
-                <p className='gray-color-text my-4'>{description}</p>
-                <div className='technologies-used'>
-                    {technologies.map(technology => <span className='gray-color-text'>{technology}</span>)}
-                </div>
-                <img src={linkImage} alt='Link image' />
-         
+            <div className='project-name'>
+                <h4 className='gray-color-text m-0'>{title}</h4>
+                <a href='#'>
+                    <img src={linkImage} alt='Link image' />
+                </a>
+            </div>
+            <p className='gray-color-text my-4'>{description}</p>
+            <div className='technologies-used'>
+                {technologies.map(technology => <span className='gray-color-text'>{technology}</span>)}
+            </div>
+            
         </div>
     )
 }
--- src/components/timelineItem/timelineItem.js
+++ src/components/timelineItem/timelineItem.js
@@ -6,7 +6,7 @@ import chevron from '../../images/chevron.svg';
 const timelineItem = ({title,time,responsibilities}) => {
     
     return (
-        <div  className="timeline-item ml-5 pl-5">
+        <div  className="timeline-item ml-5 pl-4">
 				<div className="timeline-icon">
 					
 
@@ -15,7 +15,7 @@ const timelineItem = ({title,time,responsibilities}) => {
                     <p className='gray-color-text mb-2'>{time}</p>
 					<h6 className='green-color-text mb-4'>{title}</h6>
 					
-					<div>{responsibilities ? responsibilities.map(resposibility => <span key={uuidv4()} className='gray-color-text mb-1'><img alt='chevron' src={chevron} className='mr-2' />{resposibility}</span>) : ''}</div>
+					<div>{responsibilities ? responsibilities.map(resposibility => <span key={uuidv4()} className='gray-color-text mb-3 mb-md-2'><img alt='chevron' src={chevron} className='mr-2' />{resposibility}</span>) : ''}</div>
 				</div>
 			</div>
     )
