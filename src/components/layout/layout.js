dbc8fdfeed609ee6905c53c2f2618100d968fe4c
akashdevgan <akashdevgan95@gmail.com>
2021-02-04 16:42:16 -0500

    added title

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet"
 
 //styles
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,6 +12,11 @@ import Social from '../social/social';
 const Layout = ({ children }) => {
   return (
     <React.Fragment>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Akash Devgan - Web Developer</title>
+          <link rel="canonical" href="http://mysite.com/example" />
+        </Helmet>
         <Navigation />
         <Social />
       {children}
--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import {Collapse, Navbar, NavbarBrand, Nav, NavItem} from 'reactstrap';
 import { Link } from "react-scroll";
 
-
+//logo
+import logo from '../../images/logo.svg';
 
 const Navigation = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -53,10 +54,7 @@ const Navigation = () => {
     return (
         <Navbar className={navClasses} expand='md'>
             <NavbarBrand href='/'>
-                <svg viewBox='0 0 100 100' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                    <rect x='0.5' y='0.5' width='99' height='99' stroke='#66FCF1'/>
-                    <path d='M59.4688 65.4531H40.9766L37.4609 76H26.2461L45.3008 24.8125H55.0742L74.2344 76H63.0195L59.4688 65.4531ZM43.8242 56.9102H56.6211L50.1875 37.75L43.8242 56.9102Z' fill='#66FCF1'/>
-                </svg>
+                <img src={logo} alt='Akash Devgan - Web Developer'/>
             </NavbarBrand>
             <div className={isOpen ? 'hamburger-container active d-md-none' : 'hamburger-container d-md-none'} onClick={toggle}>
                 <div className='hamburger-icon' onClick={toggle}></div>
