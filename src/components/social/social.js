e0d293958891f3d48c92dabf6ecbfea30df0f07e
akash devgan <akashdevgan95@gmail.com>
2022-03-29 05:57:24 -0400

    updated projects

--- src/components/social/social.js
+++ src/components/social/social.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React from "react"
 
 //images
-import Github from '../../images/social/github.svg';
-import Instagram from '../../images/social/instagram.svg';
-import Linkedin from '../../images/social/linkedin.svg';
+import Github from "../../images/social/github.svg"
+import Instagram from "../../images/social/instagram.svg"
+import Linkedin from "../../images/social/linkedin.svg"
+import Youtube from "../../images/social/youtube.svg"
 
 const Social = () => {
-    return (
-        <div className='social-icons d-none d-md-block'>
-            <a href='https://github.com/akashdevgan'>
-                <img alt='Github' src={Github} />
-            </a>
-            <a href='https://www.instagram.com/akash.developer/'>
-                <img alt='Instagram' src={Instagram} />
-            </a>
-            <a href='https://www.linkedin.com/in/akash24/'>
-                <img alt='LinkedIns' src={Linkedin} />
-            </a>
-        </div>
-    )
+  return (
+    <div className="social-icons d-none d-md-block">
+      <a href="https://github.com/akashdevgan">
+        <img alt="Github" src={Github} />
+      </a>
+      <a href="https://www.instagram.com/akash.developer/">
+        <img alt="Instagram" src={Instagram} />
+      </a>
+      <a href="https://www.youtube.com/channel/UCRNZrwuxh9QWzJ9PYfE0tXA">
+        <img alt="Youtube" src={Youtube} />
+      </a>
+      <a href="https://www.linkedin.com/in/akash24/">
+        <img alt="LinkedIns" src={Linkedin} />
+      </a>
+    </div>
+  )
 }
 
-export default Social;
+export default Social
