123881549e35436ea00224545630e62c82ca15a8
akash devgan <akashdevgan95@gmail.com>
2022-05-09 00:10:19 -0400

    added new project

--- src/components/miniNav/miniNav.js
+++ src/components/miniNav/miniNav.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import {Row, Col} from 'reactstrap';
+import React from "react"
+import { v4 as uuidv4 } from "uuid"
+import { Row, Col } from "reactstrap"
 
-const MiniNav = ({activeTab,onClick,navItems}) => {
-
-    return (
-        <Row>
-            <Col>
-                <div className='miniNav mb-5'>
-                    {
-                        navItems.map((navItem,i) => <button key={uuidv4()} onClick={onClick} className={activeTab === navItem ? 'active p-3':'p-3'}>{navItem}</button>)
-                    }
-                    <span className='placeholder-highlighter'></span>
-                </div>
-            </Col>
-        </Row>
-    )
+const MiniNav = ({ activeTab, onClick, navItems }) => {
+  return (
+    <Row>
+      <Col>
+        <div className="miniNav mb-5">
+          {navItems.map((navItem, i) => (
+            <button
+              key={uuidv4()}
+              onClick={onClick}
+              className={activeTab === navItem ? "" : ""}
+            >
+              {navItem}
+            </button>
+          ))}
+        </div>
+      </Col>
+    </Row>
+  )
 }
 
-export default MiniNav;
+export default MiniNav
